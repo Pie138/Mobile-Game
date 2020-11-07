7c4cef6b2d54075ee51d98dbf350766be5cac459
Pie Eip <Erreip2000@outlook.com>
2020-11-07 21:17:33 +1030

    Added timer.

---
+++ Assets/Scripts/DeadTouch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadTouch : MonoBehaviour
+{
+    private void OnTriggerEnter (Collider other)
+    {
+        GameObject.Find("Character").SendMessage("Finnish");
+    }
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public Text TimerText;
+    private float startTime;
+    private bool finnished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (finnished)
+            return;
+
+
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        TimerText.text = minutes + ":" + seconds;
+    }
+
+    public void Finnish()
+    {
+        finnished = true;
+        TimerText.color = Color.yellow;
+    }
+}
